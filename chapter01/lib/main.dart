f846a8a2868fb629e11e56e0515db8a909f797e3
MayamaTakeshi <mayamatakeshi@gmail.com>
2025-07-06 10:55:56 +0900

    Updated chapter01 and chapter02 to use flame 1.17.0

--- chapter01/lib/main.dart
+++ chapter01/lib/main.dart
@@ -7,23 +7,23 @@ import 'package:flame/game.dart';
 void main() async {
   // Create an instance of the game
   final goldRush = GoldRush();
-  
+
   // Setup Flutter widgets and start the game in full screen portrait orientation
   WidgetsFlutterBinding.ensureInitialized();
   await Flame.device.fullScreen();
   await Flame.device.setPortrait();
-  
+
   // Every is a widget in Flutter! So run the app passing the games widget here
-  runApp(
-    GameWidget(game: goldRush)
-  );
+  runApp(GameWidget(game: goldRush));
 }
 
-class GoldRush with Loadable, Game {
-
+class GoldRush extends Game {
   static const int squareSpeed = 250; // The speed that our square will animate
-  static final squarePaint = BasicPalette.green.paint(); // The color of the square
-  static final squareWidth = 100.0, squareHeight = 100.0; // The width and height of our square will be 100 x 100
+  static final squarePaint =
+      BasicPalette.green.paint(); // The color of the square
+  static final squareWidth = 100.0,
+      squareHeight =
+          100.0; // The width and height of our square will be 100 x 100
 
   late Rect squarePos; // The current position and size of our square
 
@@ -36,30 +36,33 @@ class GoldRush with Loadable, Game {
   Future<void> onLoad() async {
     super.onLoad();
 
-    // Get the width and height of our screen canvas
-    screenWidth = MediaQueryData.fromWindow(window).size.width;
-    screenHeight = MediaQueryData.fromWindow(window).size.height;
+    // Use PlatformDispatcher to get the screen size
+    final view = PlatformDispatcher.instance.views.first;
+    final size = view.physicalSize / view.devicePixelRatio;
+    screenWidth = size.width;
+    screenHeight = size.height;
 
     // Calculate the center of the screen, allowing for the adjustment for the squares size
     centerX = (screenWidth / 2) - (squareWidth / 2);
     centerY = (screenHeight / 2) - (squareHeight / 2);
 
-    // Set the initial position of the green square at the center of the screen with a size of 100 width and height 
+    // Set the initial position of the green square at the center of the screen with a size of 100 width and height
     squarePos = Rect.fromLTWH(centerX, centerY, squareWidth, squareHeight);
   }
 
   // Override this function to control what is drawn on the screen
   @override
   void render(Canvas canvas) {
-    canvas.drawRect(squarePos, squarePaint); // Draw the green square on the canvas
+    canvas.drawRect(
+        squarePos, squarePaint); // Draw the green square on the canvas
   }
 
   // Override this function to update the game state since the time elapsed since the last update
   @override
   void update(double deltaTime) {
-
     // Update the x position of the square based on the speed and direction and the time elapsed
-    squarePos = squarePos.translate(squareSpeed * squareDirection * deltaTime, 0);
+    squarePos =
+        squarePos.translate(squareSpeed * squareDirection * deltaTime, 0);
 
     // If the square reaches the side of the screen reverse the direction the square is travelling in
     if (squareDirection == 1 && squarePos.right > screenWidth) {
--- chapter02/lib/components/player.dart
+++ chapter02/lib/components/player.dart
@@ -1,16 +1,13 @@
 import 'dart:ui';
 import 'package:flame/components.dart';
-import 'package:flame/geometry.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/palette.dart';
 
-class Player extends PositionComponent with HasHitboxes, Collidable {
+class Player extends PositionComponent {
+  static const int squareSpeed = 250;
+  static final squarePaint = BasicPalette.green.paint();
+  static final squareWidth = 100.0, squareHeight = 100.0;
 
-  static const int squareSpeed = 250; // The speed that our square will animate
-  static final squarePaint = BasicPalette.green.paint(); // The color of the square
-  static final squareWidth = 100.0, squareHeight = 100.0; // The width and height of our square will be 100 x 100
-
-  // The direction our square is travelling in, 1 for left to right, -1 for right to left
   int squareDirection = 1;
   late double screenWidth, screenHeight, centerX, centerY;
 
@@ -18,44 +15,50 @@ class Player extends PositionComponent with HasHitboxes, Collidable {
   Future<void> onLoad() async {
     super.onLoad();
 
-    // Get the width and height of our screen canvas
-    screenWidth = MediaQueryData.fromWindow(window).size.width;
-    screenHeight = MediaQueryData.fromWindow(window).size.height;
+    final view = PlatformDispatcher.instance.views.first;
+    screenWidth = MediaQueryData.fromView(view).size.width;
+    screenHeight = MediaQueryData.fromView(view).size.height;
 
-    // Calculate the center of the screen, allowing for the adjustment for the squares size
     centerX = (screenWidth / 2) - (squareWidth / 2);
     centerY = (screenHeight / 2) - (squareHeight / 2);
 
-    // Set the initial position of the green square at the center of the screen with a size of 100 width and height
     position = Vector2(centerX, centerY);
     size = Vector2(squareWidth, squareHeight);
-
-    addHitbox(HitboxRectangle());
-  }
-
-  @override
-  void onCollision(Set<Vector2> points, Collidable other) {
-    if (other is ScreenCollidable) {
-      if (squareDirection == 1) {
-        squareDirection = -1;
-      } else {
-        squareDirection = 1;
-      }
-    }
   }
 
   @override
   void update(double deltaTime) {
     super.update(deltaTime);
-
-    // Update the x position of the square based on the speed and direction and the time elapsed
     position.x += squareSpeed * squareDirection * deltaTime;
+
+    // Detect collision with left or right screen edge and call onCollision
+    if (position.x <= 0) {
+      position.x = 0;
+      onCollision({position.clone()}, 'left');
+    } else if (position.x + size.x >= screenWidth) {
+      position.x = screenWidth - size.x;
+      onCollision({position.clone()}, 'right');
+    }
+  }
+
+  // Now handle the direction flip inside onCollision
+  void onCollision(Set<Vector2> points, dynamic other) {
+    if (other == 'left' || other == 'right') {
+      squareDirection *= -1;
+    }
+    // Implement other collision logic here if needed
   }
 
   @override
   void render(Canvas canvas) {
     super.render(canvas);
+    canvas.drawRect(size.toRect(), squarePaint);
+  }
 
-    renderHitboxes(canvas, paint: squarePaint);
+  @override
+  void onGameResize(Vector2 gameSize) {
+    super.onGameResize(gameSize);
+    screenWidth = gameSize.x;
+    screenHeight = gameSize.y;
   }
 }
